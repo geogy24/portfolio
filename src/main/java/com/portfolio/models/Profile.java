14b1f5fe0acaec7c44f96b8c5da9549bf437261d
Jorge Díaz <joredudiaz@gmail.com>
2021-01-30 09:40:45 -0500

    Show profile picture as base64 encoded

--- src/main/java/com/portfolio/models/Profile.java
+++ src/main/java/com/portfolio/models/Profile.java
@@ -4,11 +4,18 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.tomcat.util.codec.binary.Base64;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.sql.Blob;
+import java.util.Objects;
 
 /**
  * Profile model is using to save profile's data
@@ -55,4 +62,34 @@ public class Profile {
      */
     @Column(name = "twitter_user_id")
     private String twitterUserId;
+
+    /**
+     * Picture
+     */
+    @Column()
+    private Blob picture;
+
+    @SneakyThrows
+    public String getPicture(){
+        String base64Encoded = "";
+
+        if (Objects.nonNull(this.picture)) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            Blob blob = this.picture;
+            InputStream inputStream = blob.getBinaryStream();
+
+            int n = 0;
+
+            while ((n=inputStream.read(buffer))>=0) {
+                byteArrayOutputStream.write(buffer, 0, n);
+            }
+
+            inputStream.close();
+            byte[] encodeBase64 = Base64.encodeBase64(buffer);
+            base64Encoded = new String(encodeBase64, StandardCharsets.UTF_8);
+        }
+
+        return base64Encoded;
+    }
 }
